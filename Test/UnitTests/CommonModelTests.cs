f60453a2b8999c139570d38fc2ae6b05a81bf4f1
Denis Kucherov <dkucherov@brismoney.com>
2019-01-21 14:20:11 +0300

    ...

---
+++ Test/UnitTests/CommonModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.Common;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CommonModelTests
+    {
+        [Test]
+        [TestCase("Ivan", 20, Gender.M)]
+        public void Person(string name, int age, Gender sex)
+        {
+            var person = new Person(name, age, sex);
+
+            Assert.AreEqual(name, person.Name);
+            Assert.AreEqual(age, person.Age);
+            Assert.AreEqual(sex, person.Sex);
+        }
+    }
+}
